5d14f64ab32aa1bbf72026a2ff72d34b8928dcf6
Ulysse Gérard <thevoodoos@gmail.com>
2024-10-25 17:37:34 +0200

    command boilerplate

--- src/extension_consts.ml
+++ src/extension_consts.ml
@@ -68,6 +68,8 @@ module Commands = struct
   let ask_debug_program = ocaml_prefixed "ask-debug-program"
 
   let copy_type_under_cursor = ocaml_prefixed "copy-type-under-cursor"
+
+  let type_enclosing = ocaml_prefixed "type-enclosing"
 end
 
 module Command_errors = struct
